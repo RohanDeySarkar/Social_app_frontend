c0d6f94f544db543868f52318eb7c88b01d1118a
RohanDeySarkar <41055662+RohanDeySarkar@users.noreply.github.com>
2020-01-12 19:10:21 +0530

    Add files via upload

---
+++ build/precache-manifest.48817d04d28f4e5b46a10b41310740ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17664d026f546cb070b1bb70573c33aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d95124c4a9b62004aa0",
+    "url": "/static/css/main.eff6ecce.chunk.css"
+  },
+  {
+    "revision": "6e4c2458daf0b35e4375",
+    "url": "/static/js/2.33e3eda5.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/static/js/2.33e3eda5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d95124c4a9b62004aa0",
+    "url": "/static/js/main.2f1a7d36.chunk.js"
+  },
+  {
+    "revision": "cf3747c6f06d3e4ccdce",
+    "url": "/static/js/runtime-main.69539d94.js"
+  },
+  {
+    "revision": "e49a74f305297d9c7fd661fbf7fad0ac",
+    "url": "/static/media/icon.e49a74f3.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/react.580ffd26.png"
+  }
+]);
